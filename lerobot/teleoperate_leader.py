4c388a70009e4ddd7d121b479ed7ce962043c066
Bill Mania <bill@manialabs.us>
2025-07-22 14:23:50 -0500

    teleoperate_leader works with teleoperate_follower, across the network with two separate machines.

--- lerobot/teleoperate_leader.py
+++ lerobot/teleoperate_leader.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Script to manage the leader, sending actions via a TCP socket.
-"""
+"""Manage the leader, sending actions via a TCP socket."""
 
 import logging
 import pickle
@@ -24,17 +22,10 @@ from dataclasses import asdict, dataclass
 from pprint import pformat
 
 import draccus
-import numpy as np
 
-from lerobot.common.cameras.opencv.configuration_opencv import OpenCVCameraConfig  # noqa: F401
-from lerobot.common.cameras.realsense.configuration_realsense import RealSenseCameraConfig  # noqa: F401
 from lerobot.common.robots import (  # noqa: F401
     Robot,
-    RobotConfig,
-    koch_follower,
-    make_robot_from_config,
-    so100_follower,
-    so101_follower,
+    RobotConfig
 )
 from lerobot.common.teleoperators import (
     Teleoperator,
@@ -45,15 +36,16 @@ from lerobot.common.utils.robot_utils import busy_wait
 from lerobot.common.utils.utils import init_logging, move_cursor_up
 from lerobot.common.utils.visualization_utils import _init_rerun
 
-from .common.teleoperators import gamepad, koch_leader, so100_leader, so101_leader  # noqa: F401
+from .common.teleoperators import so101_leader  # noqa: F401
 
 client = None
 
 
 @dataclass
 class TeleoperateConfig:
+    """Provide configuration parameters."""
+
     teleop: TeleoperatorConfig
-    robot: RobotConfig
     # Limit the maximum frames per second.
     fps: int = 60
     teleop_time_s: float | None = None
@@ -74,39 +66,23 @@ def setup_socket(server_address: str, server_port: int):
 
     return client
 
+
 def teleop_loop(
     teleop: Teleoperator,
-    robot: Robot,
     fps: int,
     display_data: bool = False,
     duration: float | None = None,
     server_address: str = '0.0.0.0',
     server_port: int = 8888
 ):
+    """Loop through the teleoperation logic."""
     global client
 
-    display_len = max(len(key) for key in robot.action_features)
-    start = time.perf_counter()
     client = setup_socket(server_address, server_port)
 
     while True:
         loop_start = time.perf_counter()
         action = teleop.get_action()
-
-        """
-        if display_data:
-            observations = robot.get_observation()
-            for one_observation in observations:
-                for obs, val in one_observation.items():
-                    if isinstance(val, float):
-                        rr.log(f"observation_{obs}", rr.Scalar(val))
-                    elif isinstance(val, int):
-                        rr.log(f"observation_{obs}", rr.Scalar(val))
-                    elif isinstance(val, np.ndarray):
-                        rr.log(f"observation_{obs}", rr.Image(val), static=True)
-
-        robot.send_action(action)
-        """
 
         action_data = pickle.dumps(action)
         data_length = len(action_data)
@@ -116,42 +92,36 @@ def teleop_loop(
         dt_s = time.perf_counter() - loop_start
         busy_wait(1 / fps - dt_s)
 
-        loop_s = time.perf_counter() - loop_start
-
-        print("\n" + "-" * (display_len + 10))
-        print(f"{'NAME':<{display_len}} | {'NORM':>7}")
-        for motor, value in action.items():
-            print(f"{motor:<{display_len}} | {value:>7.2f}")
-        print(f"\ntime: {loop_s * 1e3:.2f}ms ({1 / loop_s:.0f} Hz)")
-
-        if duration is not None and time.perf_counter() - start >= duration:
-            return
-
         move_cursor_up(len(action) + 5)
 
 
 @draccus.wrap()
 def teleoperate(cfg: TeleoperateConfig):
+    """Run teleoperation."""
     init_logging()
     logging.info(pformat(asdict(cfg)))
     if cfg.display_data:
-        _init_rerun(session_name="teleoperation")
+        _init_rerun(session_name='teleoperation')
 
     teleop = make_teleoperator_from_config(cfg.teleop)
-    robot = make_robot_from_config(cfg.robot)
 
     teleop.connect()
-#    robot.connect()
 
     try:
-        teleop_loop(teleop, robot, cfg.fps, display_data=cfg.display_data, duration=cfg.teleop_time_s)
+        teleop_loop(
+            teleop,
+            cfg.fps,
+            display_data=cfg.display_data,
+            duration=cfg.teleop_time_s
+        )
+
     except KeyboardInterrupt:
         pass
+
     finally:
         teleop.disconnect()
         client.close()
-#        robot.disconnect()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     teleoperate()
